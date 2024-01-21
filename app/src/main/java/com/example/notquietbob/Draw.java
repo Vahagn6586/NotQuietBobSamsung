2aa355f3df4b246db52c6f1055ff2ddd2ea82850
Vahagn6586 <vahag6586@gmail.com>
2024-01-21 11:31:56 +0400

    Draw & DrawTHread

---
+++ app/src/main/java/com/example/notquietbob/Draw.java
@@ -0,0 +1,34 @@
+package com.example.notquietbob;
+
+import android.content.Context;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class Draw extends SurfaceView implements SurfaceHolder.Callback {
+
+    public Draw(Context context){
+        super(context);
+        getHolder().addCallback(this);
+    }
+
+    private DrawThread drawThread;
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        drawThread = new DrawThread(getContext(), getHolder());
+        drawThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
